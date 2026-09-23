cfbe8a583f6bf271cf1d7c12cddd05fe3c3a2ab3
Abdur Razzaq Sakib <abdurrazzaksakib2025@gmail.com>
2026-09-24 05:46:49 +0600

    Redme File Added

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,7 +35,9 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className={`${montserrat.className} bg-black text-white min-h-screen antialiased`}>
+      <body
+        className={`${montserrat.className} bg-black text-white min-h-screen antialiased`}
+      >
        {/* Toaster Component */}
         <Toaster 
           position="bottom-right" 
